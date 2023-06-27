fd1b950039b78b72972d95936e539af7bd92550e
abdenassar <abdenassaramimi99@gmail.com>
2023-06-27 15:31:44 +0100

    adding meal service implementation

--- src/main/java/com/lpw/getfed/services/MealService.java
+++ src/main/java/com/lpw/getfed/services/MealService.java
@@ -2,18 +2,21 @@ package com.lpw.getfed.services;
 
 import com.lpw.getfed.models.Category;
 import com.lpw.getfed.models.Meal;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-@Service
+import java.util.List;
+
+@Qualifier("meal_service")
 public interface MealService {
 
     ResponseEntity<Meal> getMealById(Long id);
     ResponseEntity<Meal> removeMealById(Long id);
-    ResponseEntity<Meal> removeMeal(MealService meal);
-    ResponseEntity<Meal> updateMeal(Long id, MealService meal);
+    ResponseEntity<String> removeMeal(Meal meal);
+    ResponseEntity<Meal> updateMeal(Long id, Meal meal);
     ResponseEntity<Page<Meal>> getMealByCategory(Category category, Pageable pageable);
-    ResponseEntity<Page<Meal>> searchMeal(String query, Pageable pageable);
+    ResponseEntity<List<Meal>> searchMeal(String query);
 }
