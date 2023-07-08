ce327ef546f806c7c5ca4f593072d2f9da40fe01
abdenassar <abdenassaramimi99@gmail.com>
2023-07-08 11:42:26 +0100

    fix loading indicator

--- frontend/common/loading/loading.tsx
+++ frontend/common/loading/loading.tsx
@@ -1,9 +1,13 @@
 import { PacmanLoader } from "react-spinners";
+import { ProgressBar } from "@hilla/react-components/ProgressBar";
 
 export const Loading = ({ size = 40 }: { size?: number }) => {
   return (
-    <div className="w-full flex justify-center min-h-[80vh]">
-      <PacmanLoader size={size} color="#FF385C" />
-    </div>
+    <>
+      <ProgressBar color="#FF385C" indeterminate={true} className="m-0" />
+      <div className="w-full flex items-center justify-center bg-background min-h-[80vh]">
+        <PacmanLoader size={size} color="#FF385C" />
+      </div>
+    </>
   );
 };
--- frontend/views/MainLayout.tsx
+++ frontend/views/MainLayout.tsx
@@ -1,8 +1,7 @@
-import Placeholder from "Frontend/components/placeholder/Placeholder.js";
 import { MenuProps, ViewRouteObject } from "Frontend/routes.js";
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
-import { Footer, Header } from "Frontend/common/index.js";
+import { Footer, Header, Loading } from "Frontend/common/index.js";
 
 type MenuRoute = ViewRouteObject &
   Readonly<{
@@ -14,7 +13,7 @@ export default function MenuOnLeftLayout() {
   return (
     <div className="text-sm sm:text-mb-xxs text-mainText">
       <Header />
-      <Suspense fallback={<Placeholder />}>
+      <Suspense fallback={<Loading />}>
         <Outlet />
       </Suspense>
       <Footer />
