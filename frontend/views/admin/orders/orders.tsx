10792a7ffe2b222e504c6136b8157a3219bea12c
abdenassar <abdenassaramimi99@gmail.com>
2023-07-12 00:54:41 +0100

    adding orders table

--- frontend/views/admin/orders/orders.tsx
+++ frontend/views/admin/orders/orders.tsx
@@ -1,9 +1,49 @@
 import * as React from "react";
+import { TableRow } from "Frontend/views/admin/orders/table-row.js";
+import Order from "Frontend/generated/com/lpw/getfed/models/Order.js";
+import OrderStatus from "Frontend/generated/com/lpw/getfed/models/enums/OrderStatus.js";
 
 export default function Orders() {
+  const data: Order[] = [
+    {
+      id: 1,
+      status: OrderStatus.IN_PROGRESS,
+      totalPrice: 200,
+      dateCreated: "12-07-2023",
+    },
+    {
+      id: 2,
+      status: OrderStatus.IN_PROGRESS,
+      totalPrice: 200,
+      dateCreated: "12-07-2023",
+    },
+    {
+      id: 3,
+      status: OrderStatus.IN_PROGRESS,
+      totalPrice: 200,
+      dateCreated: "12-07-2023",
+    },
+    {
+      id: 4,
+      status: OrderStatus.IN_PROGRESS,
+      totalPrice: 200,
+      dateCreated: "12-07-2023",
+    },
+  ];
+
   return (
-    <div className="w-full bg-white p-4 rounded-[8px] min-h-full">
-      <div className="">orders</div>
+    <div className="flex w-full flex-col gap-[32px] rounded-[8px] bg-white p-[2.222vw] sm:p-0">
+      <div className="text-xbase font-bold text-cardText sm:text-[5.340vw]">
+        Orders
+      </div>
+      Dashboard
+      <div className="">
+        <ul className="">
+          {data.map((item) => (
+            <TableRow key={`orders-table-row-${item.id}`} item={item} />
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
---
+++ frontend/views/admin/orders/table-row.tsx
@@ -0,0 +1,91 @@
+// import eye from "@/assets/icons/eye-gray-ouline.svg";
+// import arrow from "@/assets/icons/down-arrow-outline.svg";
+import { useState } from "react";
+import { ClassNames } from "Frontend/utils/classnames.js";
+import Order from "Frontend/generated/com/lpw/getfed/models/Order.js";
+import OrderStatus from "Frontend/generated/com/lpw/getfed/models/enums/OrderStatus.js";
+
+type Props = {
+  item: Order;
+};
+
+export function TableRow({ item }: Props) {
+  const [shown, setShown] = useState<boolean>(false);
+
+  return (
+    <li key={item.id} className="">
+      <div
+        onClick={() => setShown((prev) => !prev)}
+        className="flex cursor-pointer items-center justify-between border-b-[1px] border-b-[#E0E0E0] bg-[#F3F4F6] p-[14px]"
+      >
+        <div className="text-xxs text-[#A6A6A6]">
+          <div className="text-bmb-xbasease font-bold text-cardText">
+            {item.status}
+          </div>
+          <div className="">id {item.id}</div>
+          <div className="">{item.totalPrice} Mad</div>
+        </div>
+        <div className="flex gap-[8px]">
+          {/*<img src={eye} alt="eye icon" className="w-[9.709vw]" />*/}
+          {/*<img src={arrow} alt="eye icon" className="w-[9.709vw]" />*/}
+        </div>
+      </div>
+      <div
+        className={ClassNames(
+          "grid grid-rows-[0fr] overflow-hidden bg-white transition-all delay-300",
+          shown ? "grid-rows-[1fr]" : ""
+        )}
+      >
+        <div className="min-h-0">
+          <div className="flex flex-col gap-[8]">
+            <div className="flex pl-3 py-2 border-[1px] border-[#f3f3f3] items-center gap-[5.825vw]">
+              <div className="min-w-[18.204vw] font-bold text-cardText">Id</div>
+              <div className="text-[#A6A6A6]">{item.id}</div>
+            </div>
+            <div className="flex pl-3 py-2 border-[1px] border-[#f3f3f3] items-center gap-[5.825vw]">
+              <div className="min-w-[18.204vw] font-bold text-cardText">
+                Date
+              </div>
+              <div className="text-[#A6A6A6]">{item.dateCreated}</div>
+            </div>
+            <div className="flex pl-3 py-2 border-[1px] border-[#f3f3f3] items-center gap-[5.825vw]">
+              <div className="min-w-[18.204vw] font-bold text-cardText">
+                Status
+              </div>
+              <div
+                className={ClassNames(
+                  item.status === OrderStatus.DONE ? "text-[#15A024]" : "",
+                  item.status === OrderStatus.AWAITING_PAYMENT
+                    ? "text-[#E50300]"
+                    : "",
+                  item.status === OrderStatus.AWAITING ? "text-[#FF8024]" : "",
+                  item.status === OrderStatus.IN_PROGRESS
+                    ? "text-[#FF1010]"
+                    : "",
+                  item.status === OrderStatus.CANCELED ? "text-[#FF8024]" : ""
+                )}
+              >
+                {item.status}
+              </div>
+            </div>
+            <div className="flex pl-3 py-2 border-[1px] border-[#f3f3f3] items-center gap-[5.825vw]">
+              <div className="min-w-[18.204vw] font-bold text-cardText">
+                Delivery
+              </div>
+              <div className="text-[#A6A6A6]">
+                will be delivered at: {item.delivery?.estimateTime} for{" "}
+                {item.delivery?.price}
+              </div>
+            </div>
+            <div className="flex pl-3 py-2 border-[1px] border-[#f3f3f3] items-center gap-[5.825vw]">
+              <div className="min-w-[18.204vw] font-bold text-cardText">
+                Price
+              </div>
+              <div className="text-[#A6A6A6]">{item.totalPrice} Mad</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+}
