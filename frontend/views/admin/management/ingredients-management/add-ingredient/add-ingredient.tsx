281e32bb732424c58b27d544fab1bbaea6aca4c3
abdenassar <abdenassaramimi99@gmail.com>
2023-07-14 01:07:55 +0100

    add update ingredient functionality

--- frontend/views/admin/management/ingredients-management/add-ingredient/add-ingredient.tsx
+++ frontend/views/admin/management/ingredients-management/add-ingredient/add-ingredient.tsx
@@ -7,23 +7,28 @@ import { Button, ComponentLoader } from "Frontend/common/index.js";
 import * as React from "react";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import {
   CategoryEndpoint,
+  DrinkEndpoint,
   IngredientEndpoint,
 } from "Frontend/generated/endpoints.js";
 import Category from "Frontend/generated/com/lpw/getfed/models/Category.js";
 import { useUploadImage } from "Frontend/utils/hooks/use-upload-image.js";
+import useFetch from "Frontend/utils/hooks/index.js";
+import Drink from "Frontend/generated/com/lpw/getfed/models/Drink.js";
+import Ingredient from "Frontend/generated/com/lpw/getfed/models/Ingredient.js";
+import Error from "Frontend/common/error/error.js";
 
 export default function AddIngredient() {
   const [subCategories, setSubCategories] = useState([]);
-  const [loading, setLoading] = useState<boolean>(false);
+  const [saveLoading, setSaveLoading] = useState<boolean>(false);
   const { control, handleSubmit } = useForm();
   const navigate = useNavigate();
 
   useEffect(() => {
     async function getData() {
-      setLoading(true);
+      setSaveLoading(true);
       const result = await CategoryEndpoint.getSubCategories().then(
         (res) => res?.body
       );
@@ -34,28 +39,41 @@ export default function AddIngredient() {
           label: item.label,
         }))
       );
-      setLoading(false);
+      setSaveLoading(false);
     }
     getData();
   }, []);
 
-  const onSubmit = async (data: any) => {
-    setLoading(true);
-    const image = await useUploadImage(data.image).then((res) => res);
+  const {
+    state: { id },
+  } = useLocation();
+
+  const { data, loading, error } = useFetch<Ingredient>(async () => {
+    return await IngredientEndpoint.getIngrediantById(id).then(
+      (res) => res?.body
+    );
+  }, [id]);
+
+  const onSubmit = async (formdata: any) => {
+    setSaveLoading(true);
+    const image = formdata?.image
+      ? await useUploadImage(formdata.image).then((res) => res)
+      : data?.image || "";
     IngredientEndpoint.addIngrediant({
-      label: data.label,
+      label: formdata.label || data?.label,
       image,
-      price: data.price,
+      price: formdata.price || data?.price,
       subCategory: {
-        id: data.subcategory.value,
+        id: formdata.subcategory.value || data?.subCategory?.id,
       },
     }).then((res) => {
-      setLoading(false);
+      setSaveLoading(false);
       navigate("/admin/managements/ingredients");
     });
   };
 
-  if (loading) return <ComponentLoader />;
+  if (loading || saveLoading) return <ComponentLoader />;
+  if (error) return <Error />;
 
   return (
     <div>
@@ -63,6 +81,7 @@ export default function AddIngredient() {
         <form className="py-10">
           <div className="flex gap-2">
             <TextInput
+              defaultValue={data?.label}
               label="Label"
               className=""
               inputClassName="bg-background"
@@ -83,12 +102,14 @@ export default function AddIngredient() {
           </div>
           <div className="flex gap-2">
             <DropdownField
+              defaultValue={data?.subCategory?.label}
               control={control}
               label="Sub category"
               name="subcategory"
               items={subCategories}
             />
             <TextInput
+              defaultValue={data?.price}
               type="number"
               label="Price"
               className=""
